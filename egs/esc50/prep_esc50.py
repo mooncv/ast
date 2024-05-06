70769ffa3e79b74bfeedc728687ebf4a2aa7d290
moon <50862521+moon-aver@users.noreply.github.com>
2024-05-06 21:08:55 +0900

    Update prep_esc50.py

--- egs/esc50/prep_esc50.py
+++ egs/esc50/prep_esc50.py
@@ -10,6 +10,8 @@ import json
 import os
 import zipfile
 import wget
+import soundfile as sf
+import argparse
 
 # label = np.loadtxt('/data/sls/scratch/yuangong/aed-pc/src/utilities/esc50_label.csv', delimiter=',', dtype='str')
 # f = open("/data/sls/scratch/yuangong/aed-pc/src/utilities/esc_class_labels_indices.csv", "w")
@@ -29,6 +31,18 @@ import wget
 # f.close()
 #
 
+# Function to add Gaussian noise
+def add_gaussian_noise(audio, noise_factor=0.5):
+    """Adds Gaussian noise to an audio signal."""
+    noise = np.random.randn(len(audio))
+    augmented_audio = audio + noise_factor * noise
+    return augmented_audio
+
+# Argument parser for noise level
+parser = argparse.ArgumentParser(description='Prepare ESC-50 dataset with Gaussian noise.')
+parser.add_argument('--noise_level', type=float, default=0.0, help='Level of Gaussian noise to add.')
+args = parser.parse_args()
+
 def get_immediate_subdirectories(a_dir):
     return [name for name in os.listdir(a_dir) if os.path.isdir(os.path.join(a_dir, name))]
 
@@ -37,20 +51,30 @@ def get_immediate_files(a_dir):
 
 # downlooad esc50
 # dataset provided in https://github.com/karolpiczak/ESC-50
-if os.path.exists('./data/ESC-50-master') == False:
+# Download and extract ESC-50 dataset if not present
+if not os.path.exists('./data/ESC-50-master'):
     esc50_url = 'https://github.com/karoldvl/ESC-50/archive/master.zip'
     wget.download(esc50_url, out='./data/')
     with zipfile.ZipFile('./data/ESC-50-master.zip', 'r') as zip_ref:
         zip_ref.extractall('./data/')
     os.remove('./data/ESC-50-master.zip')
 
-    # convert the audio to 16kHz
+    # Define paths
     base_dir = './data/ESC-50-master/'
-    os.mkdir('./data/ESC-50-master/audio_16k/')
-    audio_list = get_immediate_files('./data/ESC-50-master/audio')
+    audio_dir = base_dir + f'audio_16k_noise{args.noise_level:.1f}/'
+    
+    # Create directories if they don't exist
+    os.makedirs(audio_dir, exist_ok=True)
+    
+    # Resample and add noise
+    audio_list = get_immediate_files(base_dir + 'audio')
     for audio in audio_list:
-        print('sox ' + base_dir + '/audio/' + audio + ' -r 16000 ' + base_dir + '/audio_16k/' + audio)
-        os.system('sox ' + base_dir + '/audio/' + audio + ' -r 16000 ' + base_dir + '/audio_16k/' + audio)
+        path = audio_dir + audio
+        print('Processing ' + audio)
+        os.system(f'sox {base_dir}/audio/{audio} -r 16000 {path}')
+        y, sr = sf.read(path)
+        y_noisy = add_gaussian_noise(y, noise_factor=args.noise_level)
+        sf.write(path, y_noisy, sr)
 
 label_set = np.loadtxt('./data/esc_class_labels_indices.csv', delimiter=',', dtype='str')
 label_map = {}
